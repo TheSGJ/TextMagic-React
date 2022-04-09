ca05301190dc8debdea385ae0e71a052b8f1168d
TheSGJ <96052650+RigelGrinMedia@users.noreply.github.com>
2022-04-09 12:17:47 +0530

    Added files

---
+++ src/components/Textform.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Textform() {
+  return (
+    <div className="container">
+        <div className="form-floating">
+         <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+         <label for="floatingTextarea2">Comments</label>
+         </div>
+    </div>
+  )
+}
