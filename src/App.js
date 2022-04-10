66f1ab97446089aacc8c9d82aa1d3249c2d72754
TheSGJ <96052650+RigelGrinMedia@users.noreply.github.com>
2022-04-10 09:09:58 +0530

    Added props!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <Navbar title="TextMagic"/>
-      <Textform />
+      <Textform heading="Enter a text, to analyse it."/>
     </>
   );
 }
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-export default function Textform() {
+export default function Textform(props) {
   return (
+    
     <div className="container">
+      <h1>{props.heading}</h1>
         <div className="form-floating">
          <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: 100}}></textarea>
          <label for="floatingTextarea2">Comments</label>
