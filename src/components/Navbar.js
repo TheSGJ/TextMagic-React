d44d3233d12fd92cfb1a028d60e760bdd2dd9a55
TheSGJ <96052650+RigelGrinMedia@users.noreply.github.com>
2022-04-10 19:13:35 +0530

    Fixed some issues!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,9 +7,7 @@ export default function Navbar(props) {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
       <a className="navbar-brand" href="/">{props.title}</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
+
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -3,11 +3,10 @@ import React from 'react'
 export default function Textform(props) {
   return (
     
-    <div className="container">
+    <div className="container my-3">
       <h1>{props.heading}</h1>
-        <div className="form-floating">
-         <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: 100}}></textarea>
-         <label for="floatingTextarea2">Comments</label>
+        <div>
+         <textarea className="form-control" rows="8" id="floatingTextarea2"></textarea>
          </div>
     </div>
   )
