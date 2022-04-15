5bf2a47214b43033a8f0d212cbaeecc8c91f71be
TheSGJ <realsgj@gmail.com>
2022-04-15 03:58:41 +0000

    Added new features for Textform component.

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,24 +1,23 @@
 import React, { useState } from "react";
 export default function Textform(props) {
   const handleUpsClick = () => {
-    console.log("Uppercase button was clicked. " + text);
     let newText = text.toUpperCase();
     setText(newText);
   };
-  const handleupOnchange = (event) => {
+  const handleOnChange = (event) => {
     setText(event.target.value);
-    console.log("on Change.");
   };
   // using the useState!
   const [text, setText] = useState("Enter text here!");
   return (
+    <>
     <div className="container my-3">
-      <h1>{props.heading}</h1>
+      <h1 className="text-center">{props.heading}</h1>
       <div class="my-3">
         <textarea
           className="form-control"
           value={text}
-          onChange={handleupOnchange}
+          onChange={handleOnChange}
           rows="8"
           id="floatingTextarea2"
         ></textarea>
@@ -27,5 +26,14 @@ export default function Textform(props) {
         Set to Uppercase
       </button>
     </div>
+    <div className="container my-3">
+      <h3 className="text-center">Text Analysis</h3>
+      <p className="text-center"><b>{text.split(" ").length}</b> words & <b>{text.length}</b> characters.</p>
+      <p className="text-center">Total Time to Read: <b>{0.008 * text.split(" ").length}</b> Minutes.</p>
+      <h4 className="text-center">Preview Text:</h4>
+      <p className="text-center">{text}</p>
+    </div>
+
+    </>
   );
 }
