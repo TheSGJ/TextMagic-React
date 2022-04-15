53326a940de1957a638c94efad74ce104eb3a84e
TheSGJ <realsgj@gmail.com>
2022-04-15 02:10:02 +0000

    Added set to uppercase function

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react'
-const [count, set] = useState("Enter text here!");
 export default function Textform(props) {
+  const handleUpsClick = () =>{
+    console.log("Uppercase button was clicked. " + text)
+    let newText = text.toUpperCase()
+    setText(newText)
+  }
+  const handleupOnchange = (event) =>{
+    setText(event.target.value)
+    console.log("on Change.")
+  }
+  // using the useState!
+  const [text, setText] = useState("Enter text here!");
   return (
     
     <div className="container my-3">
       <h1>{props.heading}</h1>
-        <div>
-         <textarea className="form-control" rows="8" id="floatingTextarea2"></textarea>
+        <div class="my-3">
+         <textarea className="form-control" value={text} onChange={handleupOnchange} rows="8" id="floatingTextarea2"></textarea>
          </div>
+         <button className="btn btn-primary" onClick={handleUpsClick}>Set to Uppercase</button>
     </div>
   )
 }
