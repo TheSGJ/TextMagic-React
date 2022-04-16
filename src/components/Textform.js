74051823c8734ce03493ea7b2901c278c2fa3cec
TheSGJ <realsgj@gmail.com>
2022-04-16 07:13:35 +0000

    Added a new function for Textform.js

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -26,6 +26,18 @@ export default function Textform(props) {
       .replace(/;}/g, "}");
     setText(newText);
   };
+
+  // Caps first letter of Word:
+  const handleCapsWord = () => {
+    const arr = text.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+    const updatedText = arr.join(" ");
+    setText(updatedText);
+  };
+
+
   //to capitalise first text of the sentence
   const handleCapFirst = () => {
     let newText = text.charAt(0).toUpperCase() + text.slice(1);
@@ -104,8 +116,11 @@ export default function Textform(props) {
         <button className="btn btn-primary mx-1 my-1" onClick={handleCapFirst}>
           Capital first Letter
         </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCapsWord}>
+          Caps first letter of Words
+        </button>
         <button className="btn btn-primary mx-1 my-1" onClick={handleSpeak}>
-          Speak
+          🔊
         </button>
         <h5 className="my-3">Dev Tools</h5>
         <button className="btn btn-primary mx-1" onClick={minifyCss}>
