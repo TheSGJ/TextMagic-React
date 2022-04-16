1472e5a4f72bb22d970fc1c2a624d8a0583a3c9d
TheSGJ <realsgj@gmail.com>
2022-04-16 06:17:04 +0000

    Added various functionalities!

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,7 @@ export default function Navbar(props) {
             <a className="nav-link" href="/">About</a>
           </li>
         </ul>
-        <form className="d-flex">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
+       
       </div>
     </div>
   </nav>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,13 +1,10 @@
 import React, { useState } from "react";
 
 export default function Textform(props) {
-  
-
   const handleClearClick = () => {
     let newText = "";
     setText(newText);
   };
-
 
   const handleUpsClick = () => {
     let newText = text.toUpperCase();
@@ -29,9 +26,45 @@ export default function Textform(props) {
       .replace(/;}/g, "}");
     setText(newText);
   };
+  //to capitalise first text of the sentence
+  const handleCapFirst = () => {
+    let newText = text.charAt(0).toUpperCase() + text.slice(1);
+    setText(newText);
+  };
+  //remove all the symbols
+  const handleTxtExtract = () => {
+    const regex = /[0-9/A-Z/a-z/ /]/g;
 
+    const letters = text.match(regex);
+    const res1 = letters.join("");
+    setText(res1);
+  };
+
+  //to extract only the numbers in the text:
+  const handleNumExtract = () => {
+    const regex = /[0-9/ /]/g;
+    const digits = text.match(regex);
+    const res = digits.join("");
+    setText(res);
+  };
+
+  const handleReverse = (event) => {
+    /* Convert string to array*/
+    let strArr = text.split("");
+    /* Reverse array*/
+    strArr = strArr.reverse();
+    /* Convert array to string*/
+    let newText = strArr.join("");
+    setText(newText);
+  };
+  // function to use Text to speech:
+  const handleSpeak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  }
   // using the useState!
-  const [text, setText] = useState("Enter some text to see preview here!");
+  const [text, setText] = useState("");
   return (
     <>
       <div className="container my-3">
@@ -40,12 +73,16 @@ export default function Textform(props) {
         <div class="my-3">
           <textarea
             className="form-control"
+            value={text}
             onChange={handleOnChange}
-            rows="8"
+            rows="6"
             id="floatingTextarea2"
           ></textarea>
         </div>
-        <button className="btn btn-outline-dark mx-1 text-center" onClick={handleClearClick}>
+        <button
+          className="btn btn-outline-dark mx-1 text-center"
+          onClick={handleClearClick}
+        >
           Clear
         </button>
         <h5 className="my-3">Utility Tools</h5>
@@ -55,7 +92,21 @@ export default function Textform(props) {
         <button className="btn btn-primary mx-1" onClick={handleLosClick}>
           Set to Lowercase
         </button>
-
+        <button className="btn btn-primary mx-1 my-1" onClick={handleTxtExtract}>
+          Extract Text
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleNumExtract}>
+          Extract Numbers
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleReverse}>
+          Reverse Text
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleCapFirst}>
+          Capital first Letter
+        </button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleSpeak}>
+          Speak
+        </button>
         <h5 className="my-3">Dev Tools</h5>
         <button className="btn btn-primary mx-1" onClick={minifyCss}>
           Minify CSS
@@ -66,6 +117,10 @@ export default function Textform(props) {
         <p className="text-center">
           <b>{text.split(" ").length}</b> words & <b>{text.length}</b>{" "}
           characters.
+        </p>
+        <p className="text-center">
+        Sentence count: <b>{text.split(/[.?!]\s/).length}</b><br />
+        Paragraph count: <b>{text.split(/\r\n|\r|\n/).length}</b>
         </p>
         <p className="text-center">
           Total Time to Read: <b>{0.008 * text.split(" ").length}</b> Minutes.
