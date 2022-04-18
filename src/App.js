61f2b9639972fe14d49fe47528c65e0b09719208
TheSGJ <60005430+TheSGJ@users.noreply.github.com>
2022-04-18 13:04:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
-import ThemeToggle from "./components/ThemeToggle";
 
 function App() {
   return (
     <>
       <Navbar title="TextMagic"/>
-      <ThemeToggle />
       <Textform heading="Enter a text, to analyse it."/>
     </>
   );
