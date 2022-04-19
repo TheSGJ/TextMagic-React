248261952b6c45afac0c1393a47f0274a73a169a
TheSGJ <realsgj@gmail.com>
2022-04-19 06:54:16 +0000

    Nav updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import "../styles/Navbar.css"
 export default function Navbar(props) {
+  const showMenu = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    window.addEventListener('scroll', () => {
+
+      hamburger.classList.remove("active");
+    
+      navMenu.classList.remove("active");
+    
+    })
+  }
+ 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: '#171c24'}}>
-    <div className="container-fluid">
-      <a className="navbar-brand" href="/">{props.title}</a>
-      
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">About</a>
-          </li>
-        </ul>
-       
-      </div>
-      
+    <nav class="navbar nav navbar-dark" style={{backgroundColor: '#171c24'}}>
+    <a href="#" class="nav-branding">{props.title}</a>
+    <ul class="nav-menu">
+        <li class="nav-item">
+            <a href="index.html" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+            <a href="about.html" class="nav-link">About</a>
+        </li>
+        <li class="nav-item">
+            <a href="https://discord.gg/zDWPHYGr5N" class="nav-link">Discord</a>
+        </li>
+    </ul>
+    <div class="hamburger" onClick={showMenu}>
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
     </div>
-  </nav>
+</nav>
   )
 }
 
