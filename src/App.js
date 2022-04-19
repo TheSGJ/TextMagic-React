28b8d7dc513f71e53bd4e386399bb241d25c346e
TheSGJ <realsgj@gmail.com>
2022-04-19 11:16:58 +0000

    few Nav fixes

--- src/App.js
+++ src/App.js
@@ -4,30 +4,20 @@ import Textform from "./components/Textform";
 import { Toggle } from './components/Toggle';
 import { useDarkMode } from './styles/useDarkMode';
 import { GlobalStyles, lightTheme, darkTheme } from './styles/globalStyles';
-import styled, { ThemeProvider } from 'styled-components';
-
-const Container = styled.div`
-  
-  
-`;
+import { ThemeProvider } from 'styled-components';
 
 function App() {
   const [ theme, toggleTheme ] = useDarkMode();
   const themeMode = theme === 'light' ? lightTheme : darkTheme;
   return (
-    
-
-      
-      
+   
       <ThemeProvider theme={themeMode}>
       <Navbar title="TextMagic"/>
-      <Container>
         <GlobalStyles />
         <div className="container my-1">
         <Toggle theme={theme} toggleTheme={toggleTheme} />
         </div>
         <Textform heading="Enter a text, to analyse it."/>
-      </Container>
     </ThemeProvider>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,35 +5,37 @@ export default function Navbar(props) {
   const showMenu = () => {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
+    
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     window.addEventListener('scroll', () => {
-
       hamburger.classList.remove("active");
-    
       navMenu.classList.remove("active");
     
     })
   }
+  const handleLinkClose = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
  
   return (
-    <nav class="navbar nav navbar-dark" style={{backgroundColor: '#171c24'}}>
-    <a href="#" class="nav-branding">{props.title}</a>
-    <ul class="nav-menu">
-        <li class="nav-item">
-            <a href="index.html" class="nav-link">Home</a>
+    <nav className="navbar nav navbar-dark" style={{backgroundColor: '#171c24'}}>
+    <a href="/" className="nav-branding">{props.title}</a>
+    <ul className="nav-menu">
+        <li className="nav-item">
+            <a className="nav-link" onClick={handleLinkClose}>Home</a>
         </li>
-        <li class="nav-item">
-            <a href="about.html" class="nav-link">About</a>
-        </li>
-        <li class="nav-item">
-            <a href="https://discord.gg/zDWPHYGr5N" class="nav-link">Discord</a>
+        <li className="nav-item">
+            <a className="nav-link" onClick={handleLinkClose}>About</a>
         </li>
     </ul>
-    <div class="hamburger" onClick={showMenu}>
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
+    <div className="hamburger" onClick={showMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
     </div>
 </nav>
   )
